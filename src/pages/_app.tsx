d1a3f2d563ac9d1ac03761e7b44f244bf39a759e
二ノ宮　大翼 <ninomiyadaisuke@ninomiyataiyokunoMacBook-Pro.local>
2021-11-04 16:37:30 +0900

    Initialized react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,44 @@
+import { useState, useEffect } from 'react';
 import { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import { QueryClient, QueryClientProvider, QueryCache } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
+import { Loading } from 'components/atoms/Utilities';
+
 import 'styles/base.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const queryCache = new QueryCache();
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+        staleTime: Infinity,
+      },
+    },
+    queryCache,
+  });
+
+  useEffect(() => {
+    router.events.on('routeChangeStart', () => setIsLoading(true));
+    router.events.on('routeChangeComplete', () => setIsLoading(false));
+    return () => {
+      router.events.off('routeChangeStart', () => setIsLoading(true));
+      router.events.off('routeChangeComplete', () => setIsLoading(false));
+    };
+  }, [setIsLoading]);
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Loading isLoading={isLoading} />
+      <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
 export default MyApp;
