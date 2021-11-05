351a922ff1d1a6de57fc46f39ca523aa7f4cb0f9
二ノ宮　大翼 <ninomiyadaisuke@ninomiyataiyokunoMacBook-Pro.local>
2021-11-05 09:53:02 +0900

    create Spacer.tsx and WidthSpacer.tsx

---
+++ src/components/atoms/Utilities/WidthSpacer.tsx
@@ -0,0 +1,16 @@
+import { VFC } from 'react';
+
+import styles from 'styles/components/atoms/width_spacer.module.scss';
+
+type Props = {
+  size: 'xxs' | 'xs' | 'sm' | 'md' | 'lg' | 'xl'
+};
+
+const WidthSpacer: VFC<Props> = (props) => {
+  const { size } = props;
+  const spacerClass = 'width_spacer-' + size;
+
+  return <div className={styles[spacerClass]} />;
+};
+
+export default WidthSpacer;
