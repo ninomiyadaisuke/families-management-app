2d99f07b49486933f0ca6f488cf6527760300eb2
二ノ宮　大翼 <ninomiyadaisuke@ninomiyataiyokunoMacBook-Pro.local>
2021-11-05 16:41:24 +0900

    create CheckBox component

---
+++ src/components/atoms/Input/CheckBox.tsx
@@ -0,0 +1,22 @@
+import { VFC } from 'react';
+import { useController, Control } from 'react-hook-form';
+
+import styles from 'styles/components/atoms/checkbox.module.scss';
+
+type Props = {
+  name: 'login_type';
+  control: Control<any, object>;
+};
+
+const CheckBox: VFC<Props> = (props) => {
+  const { name, control } = props;
+  const { field } = useController({ name, control });
+  return (
+    <div className={styles.checkbox}>
+      <input id="1" type="checkbox" {...field} />
+      <label htmlFor="1" />
+    </div>
+  );
+};
+
+export default CheckBox;
--- src/pages/sample.tsx
+++ src/pages/sample.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NextPage } from 'next';
+import { useForm } from 'react-hook-form';
+
 import { Layout } from 'components/layout';
+import { CheckBox } from 'components/atoms/Input';
 
 const Sample: NextPage = () => {
-  return <Layout pageTitle={'サンプル'}></Layout>;
+  const [checkData, setCheckData] = useState('');
+  const { handleSubmit, control } = useForm();
+
+  const onSubmit = (data: any) => {
+    setCheckData(JSON.stringify(data));
+  };
+
+  return (
+    <Layout pageTitle={'サンプル'}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <CheckBox name="login_type" control={control} />
+        <button type="submit">送信</button>
+      </form>
+      <div>{checkData}</div>
+    </Layout>
+  );
 };
 
 export default Sample;
