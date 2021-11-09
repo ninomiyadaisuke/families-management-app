0f851c624896b603ff1247badcdeddbf19fad9cd
二ノ宮　大翼 <ninomiyadaisuke@ninomiyataiyokunoMacBook-Pro.local>
2021-11-09 09:19:13 +0900

    Sample page initialization

--- src/pages/sample.tsx
+++ src/pages/sample.tsx
@@ -1,33 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { NextPage } from 'next';
-import { useForm } from 'react-hook-form';
-
 import { Layout } from 'components/layout';
-import { CheckBox } from 'components/atoms/Input';
-
-const defaultValues = {
-  login_type: false,
-};
 
 const Sample: NextPage = () => {
-  const [checkData, setCheckData] = useState('');
-  const { handleSubmit, control } = useForm({
-    defaultValues: defaultValues,
-  });
-
-  const onSubmit = (data: any) => {
-    setCheckData(JSON.stringify(data));
-  };
-
-  return (
-    <Layout pageTitle={'サンプル'}>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <CheckBox name="login_type" control={control} />
-        <button type="submit">送信</button>
-      </form>
-      <div>{checkData}</div>
-    </Layout>
-  );
+  return <Layout pageTitle={'サンプル'}></Layout>;
 };
 
 export default Sample;
