ea12f424691edd86b1f7a70e2ec40819a918068e
二ノ宮　大翼 <ninomiyadaisuke@ninomiyataiyokunoMacBook-Pro.local>
2021-11-11 11:58:10 +0900

    create IconArea component

---
+++ src/components/atoms/Images/IconArea.tsx
@@ -0,0 +1,22 @@
+import { VFC } from 'react';
+
+import Image from 'next/image';
+
+import styles from 'styles/components/atoms/icon.module.scss';
+
+type Props = {
+  path: string;
+  width: number;
+  height: number;
+};
+
+const IconArea: VFC<Props> = (props) => {
+  const { path, width, height } = props;
+  return (
+    <div className={styles.icon__base}>
+      <Image src={path} height={height} width={width} objectFit={'scale-down'} />
+    </div>
+  );
+};
+
+export default IconArea;
--- src/pages/sample.tsx
+++ src/pages/sample.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { NextPage } from 'next';
 import { Layout } from 'components/layout';
+import { IconArea } from 'components/atoms/Images';
 
 const Sample: NextPage = () => {
-  return <Layout pageTitle={'サンプル'}></Layout>;
+  return (
+    <Layout pageTitle={'サンプル'}>
+      <IconArea path={'/user_icon.png'} height={16} width={16} />
+    </Layout>
+  );
 };
 
 export default Sample;
