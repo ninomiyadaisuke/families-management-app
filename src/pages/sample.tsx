e657a2e65833e489cfdc5adb7a08b93154333e27
二ノ宮　大翼 <ninomiyadaisuke@ninomiyataiyokunoMacBook-Pro.local>
2021-11-12 08:14:49 +0900

    Initialize the sample

--- src/pages/sample.tsx
+++ src/pages/sample.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
 import { NextPage } from 'next';
 import { Layout } from 'components/layout';
-import { IconArea } from 'components/atoms/Images';
 
 const Sample: NextPage = () => {
-  return (
-    <Layout pageTitle={'サンプル'}>
-      <IconArea path={'/user_icon.png'} height={16} width={16} />
-    </Layout>
-  );
+  return <Layout pageTitle={'サンプル'}></Layout>;
 };
 
 export default Sample;
