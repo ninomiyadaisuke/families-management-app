ed10112bdc8267f6d48e226afb80bd034a997b19
二ノ宮　大翼 <ninomiyadaisuke@ninomiyataiyokunoMacBook-Pro.local>
2021-11-17 10:12:28 +0900

    Sample initialization

--- src/pages/sample.tsx
+++ src/pages/sample.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
 import { NextPage } from 'next';
 import { Layout } from 'components/layout';
-import { ImageArea } from 'components/atoms/Images';
 
 const Sample: NextPage = () => {
-  return (
-    <Layout pageTitle={'サンプル'}>
-      <ImageArea path={'/HeroSection.png'} height={871} width={1560} />
-    </Layout>
-  );
+  return <Layout pageTitle={'サンプル'}></Layout>;
 };
 
 export default Sample;
