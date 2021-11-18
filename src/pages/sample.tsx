c3b237fbc975cdb3edd294ac674b6477fd1b9354
二ノ宮　大翼 <ninomiyadaisuke@ninomiyataiyokunoMacBook-Pro.local>
2021-11-19 08:18:54 +0900

    Sample initialization

--- src/pages/sample.tsx
+++ src/pages/sample.tsx
@@ -2,30 +2,8 @@ import React from 'react';
 import { NextPage } from 'next';
 import { Layout } from 'components/layout';
 
-import { PrimaryButton } from 'components/atoms/Button';
-import { Spacer } from 'components/atoms/Utilities';
-
 const Sample: NextPage = () => {
-  const test = () => {};
-  return (
-    <Layout pageTitle={'サンプル'}>
-      <div style={{ width: '164px', height: '65px', fontSize: '20px' }}>
-        <PrimaryButton label={'送信'} type={'navy'} onClick={test} />
-      </div>
-      <Spacer size={'md'} />
-      <div style={{ width: '162px', height: '65px', fontSize: '20px' }}>
-        <PrimaryButton label={'送信'} type={'wine_red'} onClick={test} />
-      </div>
-      <Spacer size={'md'} />
-      <div style={{ width: '162px', height: '65px', fontSize: '20px' }}>
-        <PrimaryButton label={'送信'} type={'light_blue'} onClick={test} />
-      </div>
-      <Spacer size={'md'} />
-      <div style={{ width: '162px', height: '65px', fontSize: '20px' }}>
-        <PrimaryButton label={'送信'} type={'outline'} onClick={test} />
-      </div>
-    </Layout>
-  );
+  return <Layout pageTitle={'サンプル'}></Layout>;
 };
 
 export default Sample;
