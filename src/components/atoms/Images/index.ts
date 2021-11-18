a4c202901dde9ff6d3174f4fcbc4cd0d9ff5c9a6
二ノ宮　大翼 <ninomiyadaisuke@ninomiyataiyokunoMacBook-Pro.local>
2021-11-18 09:58:53 +0900

    create LogoArea comopnent and scss

--- src/components/atoms/Images/index.ts
+++ src/components/atoms/Images/index.ts
@@ -1,2 +1,3 @@
 export { default as IconArea } from './IconArea';
 export { default as ImageArea } from './ImageArea';
+export { default as LogoArea } from './LogoArea';
--- src/pages/sample.tsx
+++ src/pages/sample.tsx
@@ -2,8 +2,60 @@ import React from 'react';
 import { NextPage } from 'next';
 import { Layout } from 'components/layout';
 
+import { LogoArea } from 'components/atoms/Images';
+import { Spacer } from 'components/atoms/Utilities';
+import styles from 'styles/test.module.scss';
+
+const beforeLogin = false;
+const afterLogin = true;
+
 const Sample: NextPage = () => {
-  return <Layout pageTitle={'サンプル'}></Layout>;
+  return (
+    <Layout pageTitle={'サンプル'}>
+      <p>ヘッダーlogin前</p>
+      <Spacer size={'xs'} />
+      <div className={styles.test__header}>
+        <LogoArea path={'/main_logo_pc.png'} height={55} width={266} />
+      </div>
+      {beforeLogin ? (
+        <div className={styles.test__header_not_auth}>
+          <LogoArea path={'/f_logo.png'} height={48} width={48} />
+        </div>
+      ) : (
+        <div className={styles.test__header_auth}>
+          <LogoArea path={'/main_logo_sp.png'} height={55} width={145} />
+        </div>
+      )}
+      <Spacer size={'lg'} />
+      <p>ヘッダーlogin後</p>
+      <Spacer size={'xs'} />
+      <div className={styles.test__header}>
+        <LogoArea path={'/main_logo_pc.png'} height={55} width={266} />
+      </div>
+      {afterLogin ? (
+        <div className={styles.test__header_not_auth}>
+          <LogoArea path={'/f_logo.png'} height={48} width={48} />
+        </div>
+      ) : (
+        <div className={styles.test__header_auth}>
+          <LogoArea path={'/main_logo_sp.png'} height={55} width={145} />
+        </div>
+      )}
+      <Spacer size={'lg'} />
+      <p>footer</p>
+      <Spacer size={'xs'} />
+      <LogoArea path={'/f_logo.png'} logoType={'footer'} height={90} width={90} />
+      <Spacer size={'lg'} />
+      <p>login & signup</p>
+      <Spacer size={'xs'} />
+      <div className={styles.test__other_pc}>
+        <LogoArea path={'/main_logo_pc.png'} height={55} width={266} />
+      </div>
+      <div className={styles.test__other_sp}>
+        <LogoArea path={'/main_logo_sp_other.png'} height={66} width={280} />
+      </div>
+    </Layout>
+  );
 };
 
 export default Sample;
