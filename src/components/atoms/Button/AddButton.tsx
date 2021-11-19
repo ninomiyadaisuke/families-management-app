a7e690c249e7286dc5d74f671c35a7247cdb8e2c
二ノ宮　大翼 <ninomiyadaisuke@ninomiyataiyokunoMacBook-Pro.local>
2021-11-19 15:59:56 +0900

    fix AddButton and scss

--- src/components/atoms/Button/AddButton.tsx
+++ src/components/atoms/Button/AddButton.tsx
@@ -18,12 +18,7 @@ const AddButton: VFC<Props> = memo((props) => {
         return `${styles.button__outline}`;
     }
   })();
-  return (
-    <div className={className}>
-      <p>+</p>
-      <button onClick={onClick} />
-    </div>
-  );
+  return <div className={className} onClick={onClick} />;
 });
 
 export default AddButton;
