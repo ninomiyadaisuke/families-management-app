48164b1ae989b885163e95eddfe5bbd2f4fc5612
二ノ宮　大翼 <ninomiyadaisuke@ninomiyataiyokunoMacBook-Pro.local>
2021-11-19 13:19:01 +0900

    create AddButton component and scss

---
+++ src/components/atoms/Button/AddButton.tsx
@@ -0,0 +1,29 @@
+import { VFC, memo } from 'react';
+
+import styles from 'styles/components/atoms/add_button.module.scss';
+
+type Props = {
+  type: 'normal' | 'outline';
+  onClick: () => void;
+};
+
+const AddButton: VFC<Props> = memo((props) => {
+  const { type = '', onClick } = props;
+
+  const className = (() => {
+    switch (type) {
+      case 'normal':
+        return `${styles.button__normal}`;
+      case 'outline':
+        return `${styles.button__outline}`;
+    }
+  })();
+  return (
+    <div className={className}>
+      <p>+</p>
+      <button onClick={onClick} />
+    </div>
+  );
+});
+
+export default AddButton;
--- src/pages/sample.tsx
+++ src/pages/sample.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { NextPage } from 'next';
 import { Layout } from 'components/layout';
+import { AddButton } from 'components/atoms/Button';
 
 const Sample: NextPage = () => {
-  return <Layout pageTitle={'サンプル'}></Layout>;
+  const test = () => {};
+  return (
+    <Layout pageTitle={'サンプル'}>
+      <AddButton type={'outline'} onClick={test} />
+      <AddButton type={'normal'} onClick={test} />
+    </Layout>
+  );
 };
 
 export default Sample;
