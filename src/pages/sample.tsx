c0f0bbf4de0fad39c0ceeda8ccb9a8b8616f0e38
二ノ宮　大翼 <ninomiyadaisuke@ninomiyataiyokunoMacBook-Pro.local>
2021-11-22 13:03:30 +0900

    Sample initialization

--- src/pages/sample.tsx
+++ src/pages/sample.tsx
@@ -1,18 +1,9 @@
 import React from 'react';
 import { NextPage } from 'next';
 import { Layout } from 'components/layout';
-import { UploadButton } from 'components/atoms/Button';
 
 const Sample: NextPage = () => {
-  const test = () => {};
-  return (
-    <Layout pageTitle={'サンプル'}>
-      <UploadButton type={'file'} id={'selectedId'} buttonType={'selected'} label={'ファイルを選択'} onClick={test} />
-
-      <div style={{ margin: '100px' }} />
-      <UploadButton type={'button'} id={'uploadId'} buttonType={'upload'} label={'アップロード'} onClick={test} />
-    </Layout>
-  );
+  return <Layout pageTitle={'サンプル'}></Layout>;
 };
 
 export default Sample;
