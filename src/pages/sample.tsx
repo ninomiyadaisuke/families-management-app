c041c6e44d3ec6ac304c144d936e238468520010
二ノ宮　大翼 <ninomiyadaisuke@ninomiyataiyokunoMacBook-Pro.local>
2021-11-22 09:02:59 +0900

    sample initialization

--- src/pages/sample.tsx
+++ src/pages/sample.tsx
@@ -1,16 +1,9 @@
 import React from 'react';
 import { NextPage } from 'next';
 import { Layout } from 'components/layout';
-import { AddButton } from 'components/atoms/Button';
 
 const Sample: NextPage = () => {
-  const test = () => {};
-  return (
-    <Layout pageTitle={'サンプル'}>
-      <AddButton type={'outline'} onClick={test} />
-      <AddButton type={'normal'} onClick={test} />
-    </Layout>
-  );
+  return <Layout pageTitle={'サンプル'}></Layout>;
 };
 
 export default Sample;
