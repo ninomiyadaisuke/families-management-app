7f003437be2e8569890e983a56b82065640c974a
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2021-11-24 16:04:30 +0900

    Sample initialization and fix scss

--- src/pages/sample.tsx
+++ src/pages/sample.tsx
@@ -1,60 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { NextPage } from 'next';
 import { Layout } from 'components/layout';
-import { useForm } from 'react-hook-form';
-import { RadioInput } from 'components/atoms/Input';
-import { WidthSpacer } from 'components/atoms/Utilities';
-
-const defaultValues = {
-  transaction_type: '',
-};
 
 const Sample: NextPage = () => {
-  const [formData, setFormData] = useState('');
-  const { handleSubmit, register } = useForm({
-    defaultValues: defaultValues,
-  });
-
-  const onSubmit = (data: any) => {
-    setFormData(JSON.stringify(data));
-  };
-
-  return (
-    <Layout pageTitle={'サンプル'}>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div style={{ display: 'flex' }}>
-          <RadioInput
-            label={'お年玉'}
-            id={'otosidama'}
-            inputName={'test'}
-            disabled={false}
-            register={register}
-            name={'transaction_type'}
-          />
-          <WidthSpacer size={'lg'} />
-          <RadioInput
-            label={'プレゼント'}
-            id={'present'}
-            inputName={'test'}
-            disabled={false}
-            register={register}
-            name={'transaction_type'}
-          />
-          <RadioInput
-            label={'プレゼント'}
-            id={'test'}
-            inputName={'test'}
-            disabled={true}
-            register={register}
-            name={'transaction_type'}
-          />
-        </div>
-        <div style={{ margin: '50px' }} />
-        <button>送信</button>
-      </form>
-      <p>{formData}</p>
-    </Layout>
-  );
+  return <Layout pageTitle={'サンプル'}></Layout>;
 };
 
 export default Sample;
