ab3e700bfac2df18652429f3b27c3739d8805252
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2021-12-02 09:51:36 +0900

    create SearchInput component and scss

---
+++ src/components/atoms/Input/SearchInput.tsx
@@ -0,0 +1,30 @@
+import { VFC } from 'react';
+import { useController, Control } from 'react-hook-form';
+
+import { IconArea } from 'components/atoms/Images';
+
+import styles from 'styles/components/atoms/search_input.module.scss';
+
+type Props = {
+  type?: string;
+  placeholder: string;
+  name: string;
+  control: Control<any, object>;
+  onClick: (e?: React.BaseSyntheticEvent<object, any, any>) => Promise<void>;
+};
+
+const SearchInput: VFC<Props> = (props) => {
+  const { placeholder = '', name = '', control, onClick } = props;
+  const { field } = useController({ name, control });
+
+  return (
+    <div className={styles.search}>
+      <input type="text" placeholder={placeholder} {...field} />
+      <div className={styles.search__icon} onClick={onClick}>
+        <IconArea path={'/search_icon.png'} height={48} width={48} />
+      </div>
+    </div>
+  );
+};
+
+export default SearchInput;
--- src/pages/sample.tsx
+++ src/pages/sample.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NextPage } from 'next';
 import { Layout } from 'components/layout';
+import { SearchInput } from 'components/atoms/Input';
+import { useForm } from 'react-hook-form';
+
+const defaulteValues = {
+  test: '',
+};
+
+type Search = {
+  text: string;
+};
 
 const Sample: NextPage = () => {
-  return <Layout pageTitle={'サンプル'}></Layout>;
+  const { handleSubmit, control } = useForm({
+    defaultValues: defaulteValues,
+  });
+
+  const [formData, setFormData] = useState('');
+
+  const onSubmit = (data: Search) => {
+    setFormData(JSON.stringify(data));
+  };
+  return (
+    <Layout pageTitle={'サンプル'}>
+      <SearchInput control={control} name={'test'} placeholder={'test'} onClick={handleSubmit(onSubmit)} />
+      <p>{formData}</p>
+    </Layout>
+  );
 };
 
 export default Sample;
