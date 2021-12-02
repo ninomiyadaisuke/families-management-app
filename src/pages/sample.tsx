4e69cfea6979a91466bebb418b2a0b90feb446e9
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2021-12-02 10:20:23 +0900

    Sample initialization

--- src/pages/sample.tsx
+++ src/pages/sample.tsx
@@ -1,33 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { NextPage } from 'next';
 import { Layout } from 'components/layout';
-import { SearchInput } from 'components/atoms/Input';
-import { useForm } from 'react-hook-form';
-
-const defaulteValues = {
-  test: '',
-};
-
-type Search = {
-  text: string;
-};
 
 const Sample: NextPage = () => {
-  const { handleSubmit, control } = useForm({
-    defaultValues: defaulteValues,
-  });
-
-  const [formData, setFormData] = useState('');
-
-  const onSubmit = (data: Search) => {
-    setFormData(JSON.stringify(data));
-  };
-  return (
-    <Layout pageTitle={'サンプル'}>
-      <SearchInput control={control} name={'test'} placeholder={'test'} onClick={handleSubmit(onSubmit)} />
-      <p>{formData}</p>
-    </Layout>
-  );
+  return <Layout pageTitle={'サンプル'}></Layout>;
 };
 
 export default Sample;
