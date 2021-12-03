3a363d1b0a66df326808ac360cf9a6e41d5a1267
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2021-12-03 16:13:55 +0900

    create RegiserButton and scss

--- src/components/atoms/Button/index.ts
+++ src/components/atoms/Button/index.ts
@@ -1,3 +1,4 @@
 export { default as AddButton } from './AddButton';
 export { default as PrimaryButton } from './PrimaryButton';
+export { default as RegiserButton } from './RegiserButton';
 export { default as UploadButton } from './UploadButton';
--- src/pages/sample.tsx
+++ src/pages/sample.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NextPage } from 'next';
 import { Layout } from 'components/layout';
+import { RegiserButton } from 'components/atoms/Button';
 
 const Sample: NextPage = () => {
-  return <Layout pageTitle={'サンプル'}></Layout>;
+  const [data, setData] = useState('');
+  const test = () => {
+    setData('test');
+  };
+  return (
+    <Layout pageTitle={'サンプル'}>
+      <div style={{ marginLeft: '30px' }}>
+        <RegiserButton onClick={test} />
+      </div>
+      <p>{data}</p>
+    </Layout>
+  );
 };
 
 export default Sample;
