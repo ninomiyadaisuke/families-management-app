46860531ca48f7433653ad1a8136e1e4e3ddf2d3
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2021-12-03 16:21:45 +0900

    Sample initialization

--- src/pages/sample.tsx
+++ src/pages/sample.tsx
@@ -1,21 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { NextPage } from 'next';
 import { Layout } from 'components/layout';
-import { RegiserButton } from 'components/atoms/Button';
 
 const Sample: NextPage = () => {
-  const [data, setData] = useState('');
-  const test = () => {
-    setData('test');
-  };
-  return (
-    <Layout pageTitle={'サンプル'}>
-      <div style={{ marginLeft: '30px' }}>
-        <RegiserButton onClick={test} />
-      </div>
-      <p>{data}</p>
-    </Layout>
-  );
+  return <Layout pageTitle={'サンプル'}></Layout>;
 };
 
 export default Sample;
