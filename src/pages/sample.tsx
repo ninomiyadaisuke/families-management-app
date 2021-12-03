bc93523b039466f1fe624af2ea0017ee27b1a315
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2021-12-03 23:11:50 +0900

    Safari support

--- src/pages/sample.tsx
+++ src/pages/sample.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { NextPage } from 'next';
 import { Layout } from 'components/layout';
+import { RegiserButton } from 'components/atoms/Button';
 
 const Sample: NextPage = () => {
-  return <Layout pageTitle={'サンプル'}></Layout>;
+  return (
+    <Layout pageTitle={'サンプル'}>
+      <RegiserButton />
+    </Layout>
+  );
 };
 
 export default Sample;
