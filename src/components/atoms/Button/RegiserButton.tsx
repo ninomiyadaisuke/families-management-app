9f1fc145d0c40f62935983ced96aff88cb061d60
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2021-12-03 16:14:27 +0900

    create RegiserButton and scss

--- src/components/atoms/Button/RegiserButton.tsx
+++ src/components/atoms/Button/RegiserButton.tsx
@@ -10,12 +10,10 @@ const RegiserButton: VFC<Props> = memo((props) => {
   const { onClick } = props;
 
   return (
-    <>
-      <a onClick={onClick} className={styles.button}>
-        <span>Regiser now</span>
-        <span>登録</span>
-      </a>
-    </>
+    <a onClick={onClick} className={styles.button}>
+      <span>Regiser now</span>
+      <span>登録</span>
+    </a>
   );
 });
 
