c94cb5795ef30a1d0026924f08ce9e4d43cd4f44
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2021-12-06 15:52:19 +0900

    Sample initialization

--- src/pages/sample.tsx
+++ src/pages/sample.tsx
@@ -1,22 +1,9 @@
 import React from 'react';
 import { NextPage } from 'next';
 import { Layout } from 'components/layout';
-import { UpcommingEvents } from 'components/atoms/Utilities';
-
-const eventArray = ['井上さんの誕生日がもうすぐです', '山田さんの誕生日がもうすぐです'];
-const eventArray0 = [];
 
 const Sample: NextPage = () => {
-  return (
-    <Layout pageTitle={'サンプル'}>
-      <div style={{ margin: '30px' }}></div>
-      <div style={{ marginLeft: '10px' }}>
-        <UpcommingEvents events={eventArray} />
-        <div style={{ margin: '40px' }}></div>
-        <UpcommingEvents events={eventArray0} />
-      </div>
-    </Layout>
-  );
+  return <Layout pageTitle={'サンプル'}></Layout>;
 };
 
 export default Sample;
