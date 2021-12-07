6edb95e7b03fcc447b5d4980b9ffdd61eac4d5f5
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2021-12-08 08:17:51 +0900

    save

--- src/pages/sample.tsx
+++ src/pages/sample.tsx
@@ -1,18 +1,9 @@
 import React from 'react';
 import { NextPage } from 'next';
 import { Layout } from 'components/layout';
-import { DateSelect } from 'components/molecules/Input';
 
 const Sample: NextPage = () => {
-  return (
-    <Layout pageTitle={'サンプル'}>
-      <div>
-        <DateSelect type={'rounded'} label={'日付'} />
-        <div style={{ margin: '30px' }}></div>
-        <DateSelect type={'underLine'} label={'生年月日'} />
-      </div>
-    </Layout>
-  );
+  return <Layout pageTitle={'サンプル'}></Layout>;
 };
 
 export default Sample;
