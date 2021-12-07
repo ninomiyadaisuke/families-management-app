1a32f59837370b28a047f483c4b4a8bb28e80579
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2021-12-08 08:57:26 +0900

    The appearance is complete

--- src/pages/sample.tsx
+++ src/pages/sample.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import { NextPage } from 'next';
 import { Layout } from 'components/layout';
+import { DateSelect } from 'components/molecules/Input';
 
 const Sample: NextPage = () => {
-  return <Layout pageTitle={'サンプル'}></Layout>;
+  return (
+    <Layout pageTitle={'サンプル'}>
+      <div>
+        <DateSelect type={'rounded'} label={'日付'} />
+        <div style={{ margin: '30px' }}></div>
+        <DateSelect type={'underLine'} label={'生年月日'} />
+        <div style={{ margin: '30px' }}></div>
+      </div>
+    </Layout>
+  );
 };
 
 export default Sample;
