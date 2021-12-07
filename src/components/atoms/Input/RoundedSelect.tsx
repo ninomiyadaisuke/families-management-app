ed90c680a4183d4c64d427ee83a1e047e86599a3
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2021-12-07 11:09:17 +0900

    Added id to props

--- src/components/atoms/Input/RoundedSelect.tsx
+++ src/components/atoms/Input/RoundedSelect.tsx
@@ -5,10 +5,11 @@ import styles from 'styles/components/atoms/rounded_select.module.scss';
 type Props = {
   options: { value: string; label: string }[];
   disabled?: boolean;
+  id?: string;
 };
 
 const RoundedSelect: VFC<Props> = (props) => {
-  const { options, disabled = false } = props;
+  const { options, disabled = false, id = '' } = props;
   const [selected, setSelected] = useState(false);
 
   const changeItem = (e: ChangeEvent<HTMLSelectElement>) => {
@@ -20,7 +21,7 @@ const RoundedSelect: VFC<Props> = (props) => {
   return (
     <div className={styles.select}>
       <i />
-      <select id={'id'} onChange={changeItem} disabled={disabled} className={selected ? '' : styles.select__initial}>
+      <select id={id} onChange={changeItem} disabled={disabled} className={selected ? '' : styles.select__initial}>
         <option hidden>{options[0].label}</option>
         {options.map((option) => (
           <option key={option.label} value={option.value}>
