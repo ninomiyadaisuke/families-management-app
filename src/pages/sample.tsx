55a37c64b442a76aac948ac608bfae358e159d6d
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2021-12-13 14:23:34 +0900

    Adjust the position of labels and icons

--- src/pages/sample.tsx
+++ src/pages/sample.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { NextPage } from 'next';
 import { Layout } from 'components/layout';
 import { useForm } from 'react-hook-form';
+import { RoundedInput } from 'components/atoms/Input';
 import { SideByInput } from 'components/molecules/Input';
 
 const Sample: NextPage = () => {
@@ -12,6 +13,16 @@ const Sample: NextPage = () => {
   } = useForm();
   return (
     <Layout pageTitle={'サンプル'}>
+      <RoundedInput
+        iconIncluded={true}
+        placeholder={'姓'}
+        label={'名前'}
+        path={'/user_icon.png'}
+        type={'text'}
+        name={'first_name'}
+        control={control}
+      />
+      <div style={{ margin: '30px' }}></div>
       <SideByInput control={control} errors={errors} label={'名前'} />
     </Layout>
   );
