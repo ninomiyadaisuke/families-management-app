bef91a6b8360e6446043e9ef9d50b60a2390a790
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2021-12-14 14:44:23 +0900

    fix PrimarySelect and scss

--- src/pages/sample.tsx
+++ src/pages/sample.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
 import { NextPage } from 'next';
 import { Layout } from 'components/layout';
+import { PrimaryInput, PrimarySelect } from 'components/atoms/Input';
+import { useForm } from 'react-hook-form';
+
+const options = [
+  { label: '世帯主', value: '世帯主' },
+  { label: '配偶者', value: '配偶者' },
+  { label: '子供', value: '子供' },
+  { label: '同居人', value: '同居人' },
+];
+
+const defaultValues = {
+  type: '',
+};
 
 const Sample: NextPage = () => {
-  return <Layout pageTitle={'サンプル'}></Layout>;
+  const { handleSubmit, register } = useForm({
+    defaultValues: defaultValues,
+  });
+  return (
+    <Layout pageTitle={'サンプル'}>
+      <PrimarySelect options={options} name={'type'} register={register} label={'役割'} mandatory={true} />
+    </Layout>
+  );
 };
 
 export default Sample;
