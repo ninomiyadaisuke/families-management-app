3922ac4eaabb84e2446e6a49d7a597ffa78adb92
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2022-01-04 11:38:18 +0900

    Sample initialization

--- src/pages/sample.tsx
+++ src/pages/sample.tsx
@@ -1,16 +1,9 @@
 import React from 'react';
 import { NextPage } from 'next';
 import { Layout } from 'components/layout';
-import { MenuLink } from 'components/atoms/Utilities';
 
 const Sample: NextPage = () => {
-  return (
-    <Layout pageTitle={'サンプル'}>
-      <div style={{ height: '150px' }}>
-        <MenuLink name={'家族一覧'} path={'/contacts'} active={true} />
-      </div>
-    </Layout>
-  );
+  return <Layout pageTitle={'サンプル'}></Layout>;
 };
 
 export default Sample;
