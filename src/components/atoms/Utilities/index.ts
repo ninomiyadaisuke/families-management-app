efa55ef6168d2a6e4a1f79f70aae6d31d95dcef3
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2022-01-04 11:28:15 +0900

    create MenuLink component and scss

--- src/components/atoms/Utilities/index.ts
+++ src/components/atoms/Utilities/index.ts
@@ -1,5 +1,6 @@
 export { default as Dropdown } from './Dropdown';
 export { default as Loading } from './Loading';
+export { default as MenuLink } from './MenuLink';
 export { default as Spacer } from './Spacer';
 export { default as UpcommingEvents } from './UpcommingEvents';
 export { default as WidthSpacer } from './WidthSpacer';
