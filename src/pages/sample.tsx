567a0cf4efde25ad179ff3e6f31f9a13ac4e4f9e
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2022-01-04 09:05:43 +0900

    Sample initialization

--- src/pages/sample.tsx
+++ src/pages/sample.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
 import { NextPage } from 'next';
 import { Layout } from 'components/layout';
-import { Footer } from 'components/organisms/layout';
 
 const Sample: NextPage = () => {
-  return (
-    <Layout pageTitle={'サンプル'}>
-      <Footer />
-    </Layout>
-  );
+  return <Layout pageTitle={'サンプル'}></Layout>;
 };
 
 export default Sample;
