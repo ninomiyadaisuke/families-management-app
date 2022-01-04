4b0cd8320308f83b9e374634cbdec39f688d9173
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2022-01-04 16:47:05 +0900

    finished MenuLinks component

--- src/components/molecules/Utilities/MenuLinks.tsx
+++ src/components/molecules/Utilities/MenuLinks.tsx
@@ -1,5 +1,6 @@
-import { VFC, memo } from 'react';
+import { VFC, memo, useState } from 'react';
 import { MenuLink, Dropdown } from 'components/atoms/Utilities';
+import { useRouter } from 'next/router';
 
 import styles from 'styles/components/molecules/menu_links.module.scss';
 
@@ -8,14 +9,37 @@ const options = [
   { label: 'プレゼント一覧', path: '/list/presents' },
 ];
 
+const initialState = {
+  familyList: false,
+  register: false,
+};
+
 const MenuLinks: VFC = memo(() => {
+  const [active, setActive] = useState(initialState);
+  const router = useRouter();
+
+  const handleClick = () => {
+    if (router.pathname === '/contacts') {
+      return setActive({ familyList: true, register: false });
+    }
+    if (router.pathname === '/contacts/register') {
+      return setActive({ familyList: false, register: true });
+    }
+    return setActive(initialState);
+  };
+
   return (
     <div className={styles.links}>
-      <div className={styles.links__link}>
-        <MenuLink path={'/contacts'} name={'家族一覧'} active={true} />
+      <div className={active.familyList ? styles.links__link_active : styles.links__link}>
+        <MenuLink path={'/contacts'} name={'家族一覧'} active={active.familyList} onClick={handleClick} />
       </div>
-      <div className={styles.links__link}>
-        <MenuLink path={'/contacts'} name={'お年玉・プレゼント登録'} active={true} />
+      <div className={active.register ? styles.links__link_active : styles.links__link}>
+        <MenuLink
+          path={'/contacts/register'}
+          name={'お年玉・プレゼント登録'}
+          active={active.register}
+          onClick={handleClick}
+        />
       </div>
       <Dropdown options={options} type={'text'} />
     </div>
