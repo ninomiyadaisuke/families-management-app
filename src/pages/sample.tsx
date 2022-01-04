c21933dfcdc1dfa3452b633e93b99b864598c61b
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2022-01-04 16:54:03 +0900

    Sample initialization

--- src/pages/sample.tsx
+++ src/pages/sample.tsx
@@ -1,16 +1,9 @@
 import React from 'react';
 import { NextPage } from 'next';
 import { Layout } from 'components/layout';
-import { MenuLinks } from 'components/molecules/Utilities';
 
 const Sample: NextPage = () => {
-  return (
-    <Layout pageTitle={'サンプル'}>
-      <div style={{ height: '82px', borderTop: '1px solid', borderBottom: '1px solid' }}>
-        <MenuLinks />
-      </div>
-    </Layout>
-  );
+  return <Layout pageTitle={'サンプル'}></Layout>;
 };
 
 export default Sample;
